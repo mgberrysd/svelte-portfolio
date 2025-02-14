9d89431d3e0a1fe380746e7384daa60bee3f0cfa
mgberrysd <mgberrysd@gmail.com>
2025-02-13 16:14:34 -0800

    Updates

---
+++ _app/immutable/chunks/paths.birlzo4W.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_2qkeab)==null?void 0:s.base)??"/svelte-portfolio";var e;const t=((e=globalThis.__sveltekit_2qkeab)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.p2KKetM4.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CF-OwZn-.js";export{t as start};
